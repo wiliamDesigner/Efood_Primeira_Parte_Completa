b365eb28db7c91ea61292e391dd48f92119700f6
WiliamDev <wiliam.omura3@hotmail.com>
2025-09-08 23:36:42 -0300

    2.9
    Problema com as imagens é alguns tipos  de variavel acertada

--- src/Pages/Perfil/Modal/Modal.tsx
+++ src/Pages/Perfil/Modal/Modal.tsx
@@ -1,5 +1,15 @@
-import { Overlay, Content,  Btnfechar, Header, Organizacao, TextoeBotoes, TextoTitulo, Textodescricao, BotaoComprar,  ImagemProduto} from "../Modal/Styles";
-import pizzaImg from "../../../assets/imagem-da-pizza.png"
+import {
+  Overlay,
+  Content,
+  Btnfechar,
+  Header,
+  Organizacao,
+  TextoeBotoes,
+  TextoTitulo,
+  Textodescricao,
+  BotaoComprar,
+  ImagemProduto,
+} from "../Modal/Styles";
 import { useEffect, useState } from "react";
 
 type ModalProps = {
@@ -7,51 +17,57 @@ type ModalProps = {
   onClose: () => void;
 };
 
-type Produto={
-  id:number;
-  nome:string;
-  descricao:string;
-  capa:string;
-  preco:number;
-  porcao:number;
+type Produto = {
+  id: number;
+  nome: string;
+  descricao: string;
+  foto: string;
+  preco: number;
+  porcao: number;
 };
 
-const Modal = ({ isOpen, onClose}: ModalProps) => {
-  const[produto,setproduto]=useState<Produto | null>(null);
+const Modal = ({ isOpen, onClose }: ModalProps) => {
+  const [produto, setproduto] = useState<Produto | null>(null);
 
-  useEffect(()=>{
-    if(isOpen){
+  useEffect(() => {
+    if (isOpen) {
       fetch("https://ebac-fake-api.vercel.app/api/efood/restaurantes")
-      .then((res)=>res.json())
-      .then((data)=>{
-        const restaurante=data[0];
-        const item=restaurante.cardapio[0];
-         console.log("Produto carregado:", item);
-        setproduto(item);
-      })
-      .catch((err)=>console.error("Erro ao carregar o produto",err));
+        .then((res) => res.json())
+        .then((data) => {
+          const restaurante = data[0];
+          const item = restaurante.cardapio[0];
+          console.log("Produto carregado:", item);
+          setproduto(item);
+        })
+        .catch((err) => console.error("Erro ao carregar o produto", err));
     }
-  },[isOpen]);
+  }, [isOpen]);
 
   if (!isOpen) return null;
 
-  
   return (
     <Overlay>
       <Content>
         <Header>
-        <Btnfechar onClick={onClose}>X</Btnfechar>
+          <Btnfechar onClick={onClose}>X</Btnfechar>
         </Header>
         <Organizacao>
           {produto ? (
             <>
-              
-              <ImagemProduto src={`https://ebac-fake-api.vercel.app${produto.capa}`} alt={produto.nome} />
+              <ImagemProduto
+                src={`https://ebac-fake-api.vercel.app${produto.foto}`}alt={produto.nome}
+              />
               <TextoeBotoes>
                 <TextoTitulo>{produto.nome}</TextoTitulo>
                 <Textodescricao>{produto.descricao}</Textodescricao>
-                <Textodescricao>Serve:de {produto.porcao} </Textodescricao>
-                <BotaoComprar>Adicionar ao carrinho-R${produto.preco}</BotaoComprar>
+                <Textodescricao>Serve: de {produto.porcao} </Textodescricao>
+                <BotaoComprar>
+                  Adicionar ao carrinho -{" "}
+                  {produto.preco.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </BotaoComprar>
               </TextoeBotoes>
             </>
           ) : (
